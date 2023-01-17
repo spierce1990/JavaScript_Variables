87421b9bd75cb6e69f945226ee8b27c388916da1
Sionylen <sionylenpierce@outlook.com>
2023-01-17 01:36:30 -0600

    added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(
 // Exercise 1 and Exercise 2
 const firstName = "Sionylen";
 let lastName = `Pierce`;
-var age ="32"
+var age =32
 
 
 
@@ -19,9 +19,9 @@ let isCaseSensitive = true;
 
 // Exercise 4
 
-let price = '19.99';
+let price = 19.99;
 let isOnSale = "false";
-let salePercentage = "15";
+let salePercentage = 15;
 let stock = "0";
 let inStock = 'false';
 let selectedSize = "M";
@@ -30,7 +30,7 @@ let selectedSize = "M";
 
 let title = "Name of Wind";
 let author = "Patrick Rothfuss";
-let pageCount = '722';
-let bookMark = "456";
+let pageCount = 722;
+let bookMark = 456;
 let hasRead = "true";
 
